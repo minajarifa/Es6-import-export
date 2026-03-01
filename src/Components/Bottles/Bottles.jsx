cd905ccbe6310f70fae5c7af6fa0227a70beadcc
Your Name <minajmiha02@gmail.com>
2026-03-01 22:19:07 +0600

    added heder

---
+++ src/Components/Bottles/Bottles.jsx
@@ -0,0 +1,18 @@
+
+import { use } from "react";
+import Bottle from "./Bottle";
+
+export default function Bottles({ promiseBottles }) {
+  const bottles = use(promiseBottles);
+  console.log(bottles);
+
+  return (
+    <div className="grid grid-cols-3">
+    
+      {bottles.map((bottle) => (
+       <Bottle bottle={bottle}></Bottle>
+      ))}
+  
+    </div>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import { add, diff, divide, mult } from "../../utils/math/math"
+// import { add, diff, divide, mult } from "../../utils/math/math"
+
+import { Suspense } from "react";
+import Bottles from "../../Components/Bottles/Bottles";
 
 export default function Home() {
-    const sum = add(2,3)
-const subs = diff(5,2)
-const goonFol = mult(2,2)
-const someOne= divide(6/2)
-console.log(sum,subs,goonFol,someOne)
+//     const sum = add(2,3)
+// const subs = diff(5,2)
+// const goonFol = mult(2,2)
+// const someOne= divide(6/2)
+// console.log(sum,subs,goonFol,someOne)
+const promiseBottles = fetch(`/public/bottles.json`).then((res) =>
+    res.json(),
+  );
   return (
-    <div>Home</div>
+    <div>
+         <Suspense fallback={`Data loading commitng soon...`}>
+        <Bottles promiseBottles={promiseBottles}></Bottles>
+      </Suspense>
+    </div>
   )
 }
+
