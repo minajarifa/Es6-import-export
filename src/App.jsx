30624c0926e1e113e3606a08b908912d15d53261
Your Name <minajmiha02@gmail.com>
2026-03-01 20:46:00 +0600

    clear app.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { add, diff, mult, divide as dive } from './utils/math/math'
 
 function App() {
-  const [count, setCount] = useState(0)
 const sum = add(2,3)
 const subs = diff(5,2)
 const goonFol = mult(2,2)
@@ -13,26 +10,7 @@ const someOne= dive(6/2)
 console.log(sum,subs,goonFol,someOne)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>by awesome water bottol</h1>
     </>
   )
 }
