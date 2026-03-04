e278cb76ed3c6de04923c32e2d9b406303deaa0d
Your Name <minajmiha02@gmail.com>
2026-03-04 10:02:18 +0600

    added

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,37 +1,43 @@
 import { use, useEffect, useState } from "react";
 import Bottle from "./Bottle";
-import {  getCardFromLocalStorage } from "../../utilitis/localStarage";
+import { getCardFromLocalStorage } from "../../utilitis/localStarage";
 
 export default function Bottles({ promiseBottles }) {
   const [card, setcard] = useState([]);
   const bottles = use(promiseBottles);
-// useEffect
-useEffect(()=>{
-  const storedCartId= getCardFromLocalStorage();
-  console.log(storedCartId,bottles)
-},[Bottles])
-  
+  // useEffect
+  useEffect(() => {
+    const storedCartId = getCardFromLocalStorage();
+    for (const id of storedCartId) {
+      const storeCart = [];
+      const cartBottle = bottles.find((bottle) => bottle.id === id);
+      if (cartBottle) {
+        storeCart.push(cartBottle);
+      }
+    }
+  }, [Bottles]);
+
   const handleAddToCard = (bottol) => {
     const newCards = [...card, bottol];
     setcard(newCards);
     // save the bottole in the localstore
-    getCardFromLocalStorage(bottol.id)
+    getCardFromLocalStorage(bottol.id);
   };
   console.log("card", card);
 
   return (
-   <div>
-    <h1>Bottols: {bottles.length}</h1>
-    <h1>Added Cards: {card.length}</h1>
-     <div className="grid grid-cols-3">
-      {bottles.map((bottle) => (
-        <Bottle
-          key={bottle.id}
-          bottle={bottle}
-          handleAddToCard={handleAddToCard}
-        ></Bottle>
-      ))}
+    <div>
+      <h1>Bottols: {bottles.length}</h1>
+      <h1>Added Cards: {card.length}</h1>
+      <div className="grid grid-cols-3">
+        {bottles.map((bottle) => (
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleAddToCard={handleAddToCard}
+          ></Bottle>
+        ))}
+      </div>
     </div>
-   </div>
   );
 }
